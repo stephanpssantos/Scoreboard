5c2e09a00278d35705c60894b714291a77168e8c
Stephan Santos <stephan.pssantos@gmail.com>
2023-04-17 16:05:45 -0700

    Convert NewParty from a class to a function.

--- scoreboard.client/src/pages/NewParty.js
+++ scoreboard.client/src/pages/NewParty.js
@@ -1,49 +1,45 @@
-import React, { Component } from "react";
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import dataContext from "../dataContext";
 import './NewParty.css';
 
-class NewPartyPage extends Component {
-    constructor(props) {
-        super(props);
-        this.dateNow = new Date(Date.now());
-        this.endDateLimit = new Date();
-        this.endDateLimit.setDate(this.dateNow.getDate() + 30);
-    }
-    render() {
-        return (
-            <div className="newPartyPage">
-                <div />
-                <Formik
-                    initialValues={{ partyName: '', endDate: '' }}
-                    validate={values => {
-                        const errors = {};
-                        let parsedDate = Date.parse(values.endDate);
+function NewPartyPage({ setCurrentPage, setErrors }) {
+    let dateNow = new Date(Date.now());
+    let endDateLimit = new Date();
+    endDateLimit.setDate(dateNow.getDate() + 30);
 
-                        if (!values.partyName) {
-                            errors.partyName = 'Required';
-                        } else if (values.partyName.length < 3 || values.partyName.length > 200) {
-                            errors.partyName = 'Party name must be between 3 and 200 characters'
-                        }
+    return (
+        <div className="newPartyPage">
+            <div />
+            <Formik
+                initialValues={{ partyName: '', endDate: '' }}
+                validate={values => {
+                    const errors = {};
+                    let parsedDate = Date.parse(values.endDate);
 
-                        if (!values.endDate) {
-                            errors.endDate = 'Required'
-                        } else if (!parsedDate) {
-                            errors.endDate = 'Invalid date'
-                        } else if (parsedDate > this.endDateLimit) {
-                            errors.endDate = 'End date must be within the next 30 days'
-                        } else if (parsedDate < this.dateNow) {
-                            errors.endDate = 'End date cannot be in the past'
-                        }
+                    if (!values.partyName) {
+                        errors.partyName = 'Required';
+                    } else if (values.partyName.length < 3 || values.partyName.length > 200) {
+                        errors.partyName = 'Party name must be between 3 and 200 characters'
+                    }
 
-                        return errors;
-                    }}
-                    onSubmit={(values, { setSubmitting }) => {
-                        let newPartyOptions = {
-                            partyName: values.partyName,
-                            endDate: values.endDate
-                        };
-                        dataContext.newParty(newPartyOptions)
+                    if (!values.endDate) {
+                        errors.endDate = 'Required'
+                    } else if (!parsedDate) {
+                        errors.endDate = 'Invalid date'
+                    } else if (parsedDate > endDateLimit) {
+                        errors.endDate = 'End date must be within the next 30 days'
+                    } else if (parsedDate < dateNow) {
+                        errors.endDate = 'End date cannot be in the past'
+                    }
+
+                    return errors;
+                }}
+                onSubmit={(values, { setSubmitting }) => {
+                    let newPartyOptions = {
+                        partyName: values.partyName,
+                        endDate: values.endDate
+                    };
+                    dataContext.newParty(newPartyOptions)
                         .then((response) => {
                             return response.json();
                         })
@@ -53,33 +49,32 @@ class NewPartyPage extends Component {
                             console.log(response);
                         })
                         .catch(err => {
-                            this.props.setErrors(err.toString());
-                            this.props.setCurrentPage("errors");
+                            setErrors(err.toString());
+                            setCurrentPage("errors");
                             console.log(err);
                         });
-                    }}
-                >
-                    {({ isSubmitting }) => (
-                        <Form className="newPartyPage__container defaultInputWidth">
-                            <span>PARTY NAME</span>
-                            <ErrorMessage name="partyName" component="div" className="errorMessage" />
-                            <Field type="text" name="partyName" className="textInput centerText" placeholder="Party Name" />
-                            <span className="mt-1">END DATE</span>
-                            <ErrorMessage name="endDate" component="div" className="errorMessage" />
-                            <Field type="date" name="endDate" className="textInput centerText" />
-                            <button type="submit" disabled={isSubmitting} className="buttonInput mt-2">
-                                <strong>START</strong>
-                            </button>
-                        </Form>
-                    )}
-                </Formik>
-                <button className="defaultInputWidth buttonInput"
-                    onClick={() => this.props.setCurrentPage("landing") }>
-                    <strong>BACK</strong>
-                </button>
-            </div>
-        );
-    }
+                }}
+            >
+                {({ isSubmitting }) => (
+                    <Form className="newPartyPage__container defaultInputWidth">
+                        <span>PARTY NAME</span>
+                        <ErrorMessage name="partyName" component="div" className="errorMessage" />
+                        <Field type="text" name="partyName" className="textInput centerText" placeholder="Party Name" />
+                        <span className="mt-1">END DATE</span>
+                        <ErrorMessage name="endDate" component="div" className="errorMessage" />
+                        <Field type="date" name="endDate" className="textInput centerText" />
+                        <button type="submit" disabled={isSubmitting} className="buttonInput mt-2">
+                            <strong>START</strong>
+                        </button>
+                    </Form>
+                )}
+            </Formik>
+            <button className="defaultInputWidth buttonInput"
+                onClick={() => setCurrentPage("landing")}>
+                <strong>BACK</strong>
+            </button>
+        </div>
+    );
 }
 
 export default NewPartyPage;
